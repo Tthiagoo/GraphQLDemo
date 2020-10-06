e513bfb77cafb374dfedace823d19a9b64cc6dc3
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2020-10-06 17:01:21 -0300

    Refatorando Resolvers

--- src/index.js
+++ src/index.js
@@ -12,34 +12,19 @@ let links = [{
 }]
 
 // 1
-let idCount = links.length
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany()
-    },
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      })
-      return newLink
-    },
-  },
-}
+
+
 // 3
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: {
-    prisma,
-  }
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
 
---
+++ src/resolvers/Query.js
@@ -0,0 +1,8 @@
+function feed(parent, args, context, info) {
+  return context.prisma.link.findMany()
+}
+
+module.exports = {
+  feed,
+}
+
